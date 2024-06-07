6a2bb12a73eec7524fb08a92f76e80ba7d512998
snakefangox <14956300+snakefangox@users.noreply.github.com>
2024-06-07 13:56:34 +1200

    Initial Commit

---
+++ src/main/java/xyz/fancyteam/ajimaji/AjiMaji.java
@@ -0,0 +1,16 @@
+package xyz.fancyteam.ajimaji;
+
+import net.fabricmc.api.ModInitializer;
+
+import java.util.logging.Logger;
+
+public class AjiMaji implements ModInitializer {
+
+    public static final String MOD_ID = "aji-maji";
+    public static final Logger LOGGER = Logger.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/xyz/fancyteam/ajimaji/client/AjiMajiClient.java
@@ -0,0 +1,10 @@
+package xyz.fancyteam.ajimaji.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AjiMajiClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

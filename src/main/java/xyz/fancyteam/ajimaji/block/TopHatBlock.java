8e4541f43ab3f605e696f453bafcd5b3d465099e
Kneelawk <kneelawk@gmail.com>
2024-06-08 10:54:41 -0700

    Make top hat equippable

--- src/main/java/xyz/fancyteam/ajimaji/block/TopHatBlock.java
+++ src/main/java/xyz/fancyteam/ajimaji/block/TopHatBlock.java
@@ -1,9 +1,25 @@
 package xyz.fancyteam.ajimaji.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.Equipment;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 
-public class TopHatBlock extends Block {
+public class TopHatBlock extends Block implements Equipment {
     public TopHatBlock(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public EquipmentSlot getSlotType() {
+        return EquipmentSlot.HEAD;
+    }
+
+    @Override
+    public RegistryEntry<SoundEvent> getEquipSound() {
+        return Registries.SOUND_EVENT.getEntry(SoundEvents.BLOCK_WOOL_PLACE);
     }
 }

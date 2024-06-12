779f5a1630ff57916d79761f09b8bf8790718911
Skye Prince <spiralixp@gmail.com>
2024-06-11 20:17:50 -0700

    fix fast card movement mixin

--- src/main/java/xyz/fancyteam/ajimaji/mixin/client/ClientPlayerEntityMixin.java
+++ src/main/java/xyz/fancyteam/ajimaji/mixin/client/ClientPlayerEntityMixin.java
@@ -1,19 +1,20 @@
 package xyz.fancyteam.ajimaji.mixin.client;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+
 import net.minecraft.client.network.ClientPlayerEntity;
 
 import net.minecraft.entity.LivingEntity;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
 
 import xyz.fancyteam.ajimaji.item.AMItems;
 
 @Mixin(ClientPlayerEntity.class)
 public class ClientPlayerEntityMixin {
-    @ModifyConstant(method = "tickMovement", constant = @Constant(floatValue = 0.2f))
-    private float ajimaji$fastCardWielding(float constant) {
-        return ((LivingEntity) (Object) this).getActiveItem().isOf(AMItems.CARD_DECK) ? 1.0f : constant;
+    @ModifyExpressionValue(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isUsingItem()Z"))
+    private boolean ajimaji$fastCardWielding(boolean original) {
+        return original && !((LivingEntity) (Object) this).getActiveItem().isOf(AMItems.CARD_DECK);
     }
 }

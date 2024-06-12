09ac521bdf8e017f0ba59e7f4dd7a02fb9e6105a
Skye Prince <spiralixp@gmail.com>
2024-06-11 21:15:43 -0700

    fix the other mixin

--- src/main/java/xyz/fancyteam/ajimaji/mixin/client/ClientPlayerEntityMixin.java
+++ src/main/java/xyz/fancyteam/ajimaji/mixin/client/ClientPlayerEntityMixin.java
@@ -7,29 +7,19 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.LivingEntity;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-
-import org.spongepowered.asm.mixin.injection.Inject;
-
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import xyz.fancyteam.ajimaji.item.AMItems;
 
 @Mixin(ClientPlayerEntity.class)
 public abstract class ClientPlayerEntityMixin {
-    @Shadow
-    public abstract boolean isUsingItem();
-
     @ModifyExpressionValue(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isUsingItem()Z"))
     private boolean ajimaji$fastDeckWielding(boolean original) {
         return original && !((LivingEntity) (Object) this).getActiveItem().isOf(AMItems.CARD_DECK);
     }
 
-    @Inject(method = "canStartSprinting", cancellable = true, at = @At("HEAD"))
-    private void ajimaji$canSprintWhenWieldingDeck(CallbackInfoReturnable<Boolean> cir) {
-        if (isUsingItem() && ((LivingEntity) (Object) this).getActiveItem().isOf(AMItems.CARD_DECK)) {
-            cir.setReturnValue(true);
-        }
+    @ModifyExpressionValue(method = "canStartSprinting", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isUsingItem()Z"))
+    private boolean ajimaji$canSprintWhenWieldingDeck(boolean original) {
+        return original && !((LivingEntity) (Object) this).getActiveItem().isOf(AMItems.CARD_DECK);
     }
 }
